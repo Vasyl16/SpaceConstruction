bbb6b3ef81a182704db3c0f7c71eab4c323f1c1a
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-12 19:20:05 +0300

    fix: add checker for existing to videoHandler

--- src/js/modules/videoHandler.js
+++ src/js/modules/videoHandler.js
@@ -3,23 +3,25 @@ const videoWrapperElement = document.querySelector('.createx__video-wrapper');
 
 export const videoHandler = () => {
   // Focus the video when it starts playing
-  try {
-    videoElement.addEventListener('play', () => {
-      console.log(2);
-      videoWrapperElement.classList.add('playing');
-    });
+  if (videoElement && videoWrapperElement) {
+    try {
+      videoElement.addEventListener('play', () => {
+        console.log(2);
+        videoWrapperElement.classList.add('playing');
+      });
 
-    // Add background overlay when the video is paused or ends
-    videoElement.addEventListener('pause', () => {
-      videoWrapperElement.classList.remove('playing');
-    });
+      // Add background overlay when the video is paused or ends
+      videoElement.addEventListener('pause', () => {
+        videoWrapperElement.classList.remove('playing');
+      });
 
-    videoElement.addEventListener('ended', () => {
-      videoWrapperElement.classList.remove('playing');
-    });
+      videoElement.addEventListener('ended', () => {
+        videoWrapperElement.classList.remove('playing');
+      });
 
-    // Handle spacebar key to control video playback
-  } catch (error) {
-    console.log(error.message);
+      // Handle spacebar key to control video playback
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 };
