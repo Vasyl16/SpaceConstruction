d18e2f1fa3481620e6fbefd861ebd5182bc8a627
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-14 18:39:02 +0300

    perf: improve headerScroll.js

--- src/js/modules/headerScroll.js
+++ src/js/modules/headerScroll.js
@@ -4,16 +4,16 @@ let debounceTimeout;
 
 const onScrollEnd = () => {
   const currentScrollY = window.scrollY;
-  if (currentScrollY > 0) {
-    header.classList.add(headerActiveClass);
-  } else {
-    header.classList.remove(headerActiveClass);
-  }
+
+  currentScrollY > 0
+    ? header.classList.add(headerActiveClass)
+    : header.classList.remove(headerActiveClass);
 };
 
 export const handleHeaderScroll = () => {
   window.addEventListener('scroll', () => {
     clearTimeout(debounceTimeout);
+
     debounceTimeout = setTimeout(() => {
       requestAnimationFrame(onScrollEnd);
     }, 30);
