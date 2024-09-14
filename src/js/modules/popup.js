affc4e33d204781e22d0c677a4dca989a3b5f124
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-14 19:20:03 +0300

    perf: improve a popup.js

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -1,9 +1,12 @@
+import { blockScroll } from './blockScroll';
+
 const popupItems = [
   {
     buttonClass: 'our-positions__button-link_subscribe',
     popupClass: 'subscribe-popup-our-positions',
     closeIconClass: 'subscribe-popup-our-positions__cros-icon',
   },
+
   {
     buttonClass: 'our-positions__button-link_send',
     popupClass: 'send-popup-our-positions',
@@ -17,25 +20,27 @@ export const handlePopupItems = () => {
     const popup = document.querySelector(`.${item.popupClass}`);
     const closeIcon = document.querySelector(`.${item.closeIconClass}`);
 
-    if (button && popup && closeIcon) {
-      button.addEventListener('click', () => {
-        const viewportHeight = window.innerHeight;
-        const popupHeight = popup.offsetHeight;
-
-        popup.classList.add(item.popupClass + '_a');
-        document.body.style.overflow = 'hidden';
-
-        if (popupHeight > viewportHeight) {
-          popup.style.alignItems = 'flex-start';
-          popup.style.minHeight = 'auto';
-          popup.style.height = '100%';
-        } else {
-        }
-      });
-      closeIcon.addEventListener('click', () => {
-        popup.classList.remove(item.popupClass + '_a');
-        document.body.style.overflow = '';
-      });
+    if (!button && !popup && !closeIcon) {
+      return;
     }
+
+    button.addEventListener('click', () => {
+      const viewportHeight = window.innerHeight;
+      const popupHeight = popup.offsetHeight;
+
+      popup.classList.add(item.popupClass + '_a');
+
+      blockScroll(true);
+
+      if (popupHeight > viewportHeight) {
+        popup.classList.add(item.popupClass + '_small-height');
+      }
+    });
+
+    closeIcon.addEventListener('click', () => {
+      popup.classList.remove(item.popupClass + '_a');
+
+      blockScroll(false);
+    });
   });
 };
