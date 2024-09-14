0f1d524dcb7d692d5defb30bf04f9ddebef39337
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-14 19:54:39 +0300

    fix: improve spoiler.js

--- src/js/modules/spoiler.js
+++ src/js/modules/spoiler.js
@@ -1,6 +1,5 @@
-const MOBILE_BREAKPOINT = 767.98;
-
 // Footer spoiler constants
+const MOBILE_BREAKPOINT = 767.98;
 const spoilersFooter = document.querySelectorAll('.spoiler-footer');
 const titleFooterClass = 'spoiler-footer-title';
 const contentFooterClass = 'spoiler-footer-content';
@@ -21,7 +20,7 @@ const spoilersAll = [
     spoiler: spoilersWeOffer,
     title: titleWeOfferClass,
     content: contentWeOfferClass,
-    resolution: false, // No resolution limit for these spoilers
+    resolution: false,
   },
 ];
 
@@ -46,11 +45,11 @@ const collapseOtherSpoilers = (
       const otherContent = otherSpoiler.querySelector('.' + contentClass);
       const otherTitle = otherSpoiler.querySelector('.' + titleClass);
 
-      otherContent.style.maxHeight = '0';
-      otherContent.style.opacity = '0';
       otherContent.classList.remove(contentClass + '_a');
-
       otherTitle.classList.remove(titleClass + '_a');
+
+      otherContent.style.maxHeight = '';
+      otherContent.style.opacity = '';
     }
   });
 };
@@ -58,69 +57,56 @@ const collapseOtherSpoilers = (
 const collapseAllSpoilers = (currentSpoiler, contentClass, titleClass) => {
   const currentContent = currentSpoiler.querySelector('.' + contentClass);
   const currentTitle = currentSpoiler.querySelector('.' + titleClass);
+  const ifInitialSpoilerActive = currentSpoiler
+    .querySelector('.' + contentClass)
+    .classList.contains(contentClass + '_active');
 
-  if (
-    currentSpoiler
-      .querySelector('.' + contentClass)
-      .classList.contains(contentClass + '_active')
-  ) {
+  if (ifInitialSpoilerActive) {
     currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
     currentContent.style.opacity = '1';
     currentContent.classList.remove(contentClass + '_active');
-
-    return true;
+    return;
   }
 
-  currentContent.style.maxHeight = '0';
-  currentContent.style.opacity = '0';
   currentContent.classList.remove(contentClass + '_a');
-
   currentTitle.classList.remove(titleClass + '_a');
 };
 
-// Function to initialize spoilers
 const spoilerFun = (spoilers, titleClass, contentClass, resolutionMax) => {
   spoilers.forEach((spoiler) => {
+    const isUnderResolution =
+      resolutionMax === false || window.innerWidth < resolutionMax;
     const title = spoiler.querySelector('.' + titleClass);
     const content = spoiler.querySelector('.' + contentClass);
-
-    // Remove any existing event listener and replace title with a fresh node to prevent duplicate event listeners
     const newTitle = title.cloneNode(true);
+
     title.replaceWith(newTitle);
+
     const updatedTitle = spoiler.querySelector('.' + titleClass);
 
-    try {
-      const isUnderResolution =
-        resolutionMax === false || window.innerWidth < resolutionMax;
+    collapseAllSpoilers(spoiler, contentClass, titleClass);
 
-      collapseAllSpoilers(spoiler, contentClass, titleClass);
+    if (isUnderResolution) {
+      updatedTitle.addEventListener('click', () => {
+        collapseOtherSpoilers(spoilers, spoiler, contentClass, titleClass);
 
-      if (isUnderResolution) {
-        updatedTitle.addEventListener('click', () => {
-          // Toggle clicked spoiler, collapse others
-          collapseOtherSpoilers(spoilers, spoiler, contentClass, titleClass);
-
-          // Toggle current spoiler
-          toggleSpoiler(content, updatedTitle, contentClass, titleClass);
-        });
-      } else {
-        // If resolution exceeds the limit, expand all spoilers
-        spoilers.forEach((otherSpoiler) => {
-          const otherContent = otherSpoiler.querySelector('.' + contentClass);
-          otherContent.style.maxHeight = otherContent.scrollHeight + 'px';
-          otherContent.style.opacity = '1';
-          otherSpoiler
-            .querySelector('.' + titleClass)
-            .classList.remove(titleClass + '_a');
-        });
-      }
-    } catch (error) {
-      console.log(error.message);
+        toggleSpoiler(content, updatedTitle, contentClass, titleClass);
+      });
+      return;
     }
+
+    spoilers.forEach((otherSpoiler) => {
+      const otherContent = otherSpoiler.querySelector('.' + contentClass);
+
+      otherContent.style.maxHeight = '';
+      otherContent.style.opacity = '';
+      otherSpoiler
+        .querySelector('.' + titleClass)
+        .classList.remove(titleClass + '_a');
+    });
   });
 };
 
-// Function to set up all spoilers
 const setUpAllSpoilers = (spoilersAll) => {
   for (const spoilerObj of spoilersAll) {
     spoilerFun(
@@ -132,10 +118,7 @@ const setUpAllSpoilers = (spoilersAll) => {
   }
 };
 
-// Debounce function to prevent excessive calls on window resize
-
 // Initial setup and window resize event listener
-
 export const initAllSpoilers = () => {
   window.addEventListener('resize', () => {
     setUpAllSpoilers(spoilersAll);
