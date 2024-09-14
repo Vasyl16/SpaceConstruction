3065d9a3554bc65cc4677d5f767e86b1dec701a2
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-14 18:45:10 +0300

    perf: improve videoHandler

--- src/js/modules/videoHandler.js
+++ src/js/modules/videoHandler.js
@@ -2,26 +2,19 @@ const videoElement = document.querySelector('.createx__video');
 const videoWrapperElement = document.querySelector('.createx__video-wrapper');
 
 export const videoHandler = () => {
-  // Focus the video when it starts playing
-  if (videoElement && videoWrapperElement) {
-    try {
-      videoElement.addEventListener('play', () => {
-        console.log(2);
-        videoWrapperElement.classList.add('playing');
-      });
-
-      // Add background overlay when the video is paused or ends
-      videoElement.addEventListener('pause', () => {
-        videoWrapperElement.classList.remove('playing');
-      });
-
-      videoElement.addEventListener('ended', () => {
-        videoWrapperElement.classList.remove('playing');
-      });
-
-      // Handle spacebar key to control video playback
-    } catch (error) {
-      console.log(error.message);
-    }
+  if (!videoElement && !videoWrapperElement) {
+    return;
   }
+
+  videoElement.addEventListener('play', () => {
+    videoWrapperElement.classList.add('playing');
+  });
+
+  videoElement.addEventListener('pause', () => {
+    videoWrapperElement.classList.remove('playing');
+  });
+
+  videoElement.addEventListener('ended', () => {
+    videoWrapperElement.classList.remove('playing');
+  });
 };
