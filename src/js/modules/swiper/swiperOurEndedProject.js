47c2d100506a1abf2ff98a640f9ed5662d0fdc13
Yurchuk Vasyl <mira07081984@gmail.com>
2024-09-23 15:44:13 +0300

    refactor(refactor some code):

--- src/js/modules/swiper/swiperOurEndedProject.js
+++ src/js/modules/swiper/swiperOurEndedProject.js
@@ -1,24 +1,26 @@
 import Swiper from 'swiper/bundle';
 
 // swiper thumbnail-our-ended-project
-const swiperThumbnailOurEndedProject = '.swiper-thumbnail-our-ended-project';
 
-// const config thumbnail-our-ended-project
-export const swiperThumbnailOurEndedProjectLabel = new Swiper(
-  swiperThumbnailOurEndedProject,
-  {
-    slidesPerView: 'auto', // Automatically size each thumbnail based on its content width
-    slideToClickedSlide: true,
-    spaceBetween: 20,
-  }
-);
+const swiperThumbnailOurEndedProject = '.swiper-thumbnail-our-ended-project';
 
 // swiper main vars
 const swiperOurEndedProject = '.swiper-our-ended-project';
 const swiperOurEndedProjectButtonPrev = '.swiper-our-ended-project-button-prev';
 const swiperOurEndedProjectButtonNext = '.swiper-our-ended-project-button-next';
 
+// const config thumbnail-our-ended-project
+
 export const swiperOurEndedProjectLabel = () => {
+  const swiperThumbnailOurEndedProjectLabel = new Swiper(
+    swiperThumbnailOurEndedProject,
+    {
+      slidesPerView: 'auto', // Automatically size each thumbnail based on its content width
+      slideToClickedSlide: true,
+      spaceBetween: 20,
+    }
+  );
+
   new Swiper(swiperOurEndedProject, {
     slidesPerView: 1,
     speed: 700,
